115b3f506382a7647c94fe03a5708634d3a62e5e
Dakelnut <r.y.o@list.ru>
2017-10-30 18:49:39 +0300

    add combobox for schema and tables

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
-
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTableWidgetItem
 from PyQt5.uic import *
 from PyQt5.QtWidgets import QApplication, QMessageBox
 from PyQt5 import QtWidgets
@@ -8,15 +9,17 @@ import sys
 
 
 
-class ConnectionDialog(QtWidgets.QMainWindow):
+class ConnectionDialog(QtWidgets.QDialog):
 
-    def __init__(self):
+    def __init__(self, parent=None):
+
+        QtWidgets.QDialog.__init__(self)
         self.dialog_window = loadUi("dialog.ui")
         self.buttonBox = self.dialog_window.buttonBox
         self.buttonBox.accepted.connect(self.apply)
         self.buttonBox.rejected.connect(self.reject)
         self.conn_string = ""
-        self.dialog_window.show()
+
 
 
     def apply(self):
@@ -52,9 +55,13 @@ class ConnectionDialog(QtWidgets.QMainWindow):
 
 
 
-class DdWindow():
+class DdWindow( QtWidgets.QMainWindow):
 
-    def __init__(self,cn_string):
+    def __init__(self, cn_string, parent=None):
+        self.flag = True
+        self.choosen_schema = None
+        self.choosen_table = None
+        QtWidgets.QMainWindow.__init__(self)
         self.init_connection(cn_string)
         self.main_window = loadUi("mwindow.ui")
         self.main_table = self.main_window.tableWidget
@@ -63,20 +70,77 @@ class DdWindow():
         self.search_button = self.main_window.search
         self.help_button = self.main_window.help
         self.table_chooser = self.main_window.table_chooser
-        self.init_table_chooser(self.table_chooser)
-        self.main_window.show()
+        self.schema_chooser = self.main_window.schema_chooser
 
-    def init_table_chooser(self, tb_chooser):
-        self.cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
+        self.init_schema_chooser(self.schema_chooser)
+        self.init_table_chooser(self.table_chooser)
+        # self.fill_table()
+
+
+
+    def init_table_chooser(self,tb_chooser):
+
+        self.cursor.execute(("SELECT TABLE_NAME  from information_schema.tables where table_schema= '{0}'").format(self.choosen_schema))
+
+
         table_names = [name[0] for name in self.cursor.fetchall()]
-        for table in table_names:
-            tb_chooser.addItem(table)
+        if len(table_names) == 0:
+            tb_chooser.addItem(str(None))
+        else:
+            for table in table_names:
+                tb_chooser.addItem(table)
+
+
+
+    def init_schema_chooser(self, sh_chooser):
+        self.cursor.execute("select schema_name from information_schema.schemata where  schema_name !~ '^(pg_|sql_)'  AND  schema_name !~'information_schema';")
+        schema_names = [name[0] for name in self.cursor.fetchall()]
+        for schema in schema_names:
+            sh_chooser.addItem(schema)
+
+        self.choosen_schema = self.schema_chooser.currentText()
+
+        # sh_chooser.activated.connect(self.init_table_chooser)
 
     def init_connection(self,conn_string):
         self.conn = psycopg2.connect(conn_string)
         self.cursor = self.conn.cursor()
 
 
+    def fill_table(self):
+        # self.cursor.execute(("SELECT * FROM  {0}").format(self.table_names[1]))
+        self.cursor.execute(("SELECT * FROM sc.{0}").format(self.table_names[0]))
+        # self.cursor.execute(("select a.attname, format_type(a.atttypid, a.atttypmod) from pg_attribute a where attname = 'geog'"))
+        print(self.cursor.description)
+        # a = [elem for elem in self.cursor.fetchall()]
+        # print( a)
+        #
+        # colnames = [desc[0] for desc in self.cursor.description]
+        # print(colnames)
+        #
+        #
+        #
+        # self.main_table.setColumnCount(2)
+        # self.main_table.insertRow(self.main_table.rowCount())
+        #
+        # newitem = QTableWidgetItem(str(a[0][0]))
+        # print(a[0][0])
+        # self.main_table.setItem(0, 0, newitem)
+        # self.main_table.item(0, 0).setTextAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        # newitem = QTableWidgetItem(a[0][1])
+        # self.main_table.setItem(0, 1, newitem)
+        # self.main_table.item(0, 1).setTextAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        # self.main_table.setHorizontalHeaderLabels(colnames)
+        #
+        # # for n, meaning in  enumerate(a):
+        # #     self.main_table.insertRow(self.main_table.rowCount())
+        # #     print(n,"  ",meaning)
+        # #     for m, item in enumerate(meaning):
+        # #         print("\t",m, "  ", item)
+        # #         newitem = QTableWidgetItem(item)
+        # #         self.main_table.setItem(n, m, newitem)
+        # #         self.main_table.item(n, m).setTextAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        # print(self.main_table.item(0,0).text())
 
 
 
@@ -89,9 +153,13 @@ if __name__ == "__main__":
     # window.open()
     # window.show()
     app = QApplication(sys.argv)
-    test = ConnectionDialog()
+    # test = ConnectionDialog()
+    # test.dialog_window.exec_()
+    # conn_string = "host='localhost'dbname='postgres' user='postgres' password='root'"
     conn_string = "host='localhost'dbname='test38' user='postgres' password='root'"
-    window = DdWindow(conn_string)
 
+    if conn_string!="":
+        window = DdWindow(conn_string)
+        window.main_window.show()
     sys.exit(app.exec_())
 
